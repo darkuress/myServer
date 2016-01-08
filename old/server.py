79b90e2cbceacee11adc236b5167b43af798af16
darkuress <jhwang.ani@gmail.com>
2016-01-08 23:23:51 +0000

    first commit
    first commit

---
+++ old/server.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python           # This is server.py file
+
+import socket               # Import socket module
+
+def runServer():
+    s = socket.socket()         # Create a socket object
+    host = ''                   # Get local machine name
+    port = 5001                 # Reserve a port for your service.
+    s.bind((host, port))        # Bind to the port
+
+    s.listen(5)                 # Now wait for client connection.
+    while True:
+       c, addr = s.accept()     # Establish connection with client.
+       print 'Got connection from', addr
+       c.send('Thank you for connecting')
+       c.close()                # Close the connection
+
+if __name__ == "__main__":
+    print 'running......'
+    runServer()
